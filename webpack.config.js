e7eda5a7fc01af2395818efbc36f5e2f6c042894
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 19:41:29 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:9000/pilot/common/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "common",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./Bookmark": "./src/components/bookmark/Bookmark.jsx",
+        "./BreadcrumbNav": "./src/components/breadcrumbs/BreadcrumbNav.jsx",
+        "./Copyright": "./src/components/Copyright.jsx",
+       "./HeadAppBar": "./src/components/HeadAppBar.jsx",
+        "./NotiSummaryBar" : "./src/components/NotiSummaryBar.jsx",
+        "./StyledSearchBar": "./src/components/StyledSearchBar.jsx",
+        "./CustomIcons": "./src/components/icons/CustomIcons.jsx",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});

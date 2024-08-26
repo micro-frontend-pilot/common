98ffa0aaf1a9dd6846f8c9ae325836a2ef484f03
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 07:43:55 +0900

    Create AngularTestComponent.jsx

---
+++ src/components/AngularTestComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const AngularTestComponent = () => {
+    return (
+        <div id="react-test-component">
+            angular test component
+        </div>
+    )
+}
+
+// const app = ReactDOM.createRoot(document.getElementById("app"));
+// app.render(<AngularTestComponent />);
+
+export default AngularTestComponent;

b8153e44d1db4cead5c1fdc6be4635d684f4ae3d
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 09:42:51 +0900

    Update AngularTestComponent.jsx

--- src/components/AngularTestComponent.jsx
+++ src/components/AngularTestComponent.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-const AngularTestComponent = () => {
-    return (
-        <div id="react-test-component">
-            angular test component
-        </div>
-    )
+import PropTypes from 'prop-types';
+
+class AngularTestComponent extends React.Component {
+    render() {
+        return (
+            <div id="react-test-component">
+                message: { this.props.message }
+            </div>
+        )
+    }
 }
 
 // const app = ReactDOM.createRoot(document.getElementById("app"));
 // app.render(<AngularTestComponent />);
+
+AngularTestComponent.propTypes = {
+    message: PropTypes.string
+}
 
 export default AngularTestComponent;
